668fecc6e330adb02793a78b6367b62e0a62fea1
Balystick <balystick74@icloud.com>
2024-12-09 13:11:57 +0100

    Initial Commit

---
+++ ZakFit_front/ZakFit_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFit_frontApp.swift
+//  ZakFit_front
+//
+//  Created by Aurélien on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZakFit_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
